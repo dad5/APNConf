f33c073d8178936c3c3f0bde6db9ca4cc541c27b
Lukas Reschke <lukas@statuscode.ch>
2012-08-03 19:00:08 +0200

    First commit

---
+++ apnconf/apnconf.go
@@ -0,0 +1,74 @@
+package app
+
+import (
+	"bytes"
+	"encoding/xml"
+	"html/template"
+	"library/cache"
+	"io/ioutil"
+	"library/render"
+	"encoding/gob"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/list", list)
+}
+
+type APN struct {
+	Carrier  string `xml:"carrier,attr"`
+	APN      string `xml:"apn,attr"`
+	User     string `xml:"user,attr"`
+	Password string `xml:"password,attr"`
+	Proxy    string `xml:"proxy,attr"`
+	Port     int    `xml:"port,attr"`
+	MMSC     string `xml:"mmsc,attr"`
+	MMSProxy string `xml:"mmsproxy,attr"`
+	MMSPort  string `xml:"mmsport,attr"`
+	MCC		string `xml:"mcc,attr"`
+	MNC  string `xml:"mnc,attr"`
+}
+type APNStruct struct {
+	APN []APN `xml:"apn"`
+}
+
+// Function to get all APN
+func getAPN (r *http.Request)  []APN {
+// Check if we have the item in the cache
+	cachedItem, cacheStatus := cache.GetCache(r, "getAPN")
+	if cacheStatus == true {
+		var apn []APNStruct
+		pAPN := bytes.NewBuffer(cachedItem)
+		decAPN := gob.NewDecoder(pAPN)
+		decAPN.Decode(&apn)
+		return apn[0].APN
+	}
+
+	// Read out 
+	data, _ := ioutil.ReadFile("apns-conf.xml")
+	var apn []APNStruct
+	xml.Unmarshal(data, &apn)
+
+	// Add to cache
+	mModels := new(bytes.Buffer) //initialize a *bytes.Buffer
+	encModels := gob.NewEncoder(mModels)
+	encModels.Encode(apn)
+	cache.AddCache(r, "getAPN", mModels.Bytes())
+
+	return apn[0].APN
+}
+
+// Home
+func home(w http.ResponseWriter, r *http.Request) {
+	passedTemplate := new(bytes.Buffer)
+	template.Must(template.ParseFiles("apnconf/templates/home.html")).Execute(passedTemplate, getAPN(r))
+	render.Render(w, r, passedTemplate)
+}
+
+// List of all APNs
+func list(w http.ResponseWriter, r *http.Request) {
+	passedTemplate := new(bytes.Buffer)
+	template.Must(template.ParseFiles("apnconf/templates/list.html")).Execute(passedTemplate, getAPN(r))
+	render.Render(w, r, passedTemplate)
+}
---
+++ library/render/render.go
@@ -0,0 +1,31 @@
+// This package is called to render the template.
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"text/template" // WARNING: template/html removes the <!--[if lt IE 9]>, so we're using text/template - but this is just applied over the header and the footer so it should be safe to use.
+)
+
+/*
+ * Renders a template (passedTemplate) and inserts the header and footer
+ */
+func Render(w http.ResponseWriter, r *http.Request, passedTemplate *bytes.Buffer, Statuscode ...int) {
+	// Add some HTTP Headers
+	w.Header().Set("X-Frame-Options", "DENY")           // Deny frames
+	w.Header().Set("X-XSS-Protection", "1; mode=block") // XSS Protection
+	w.Header().Set("X-Content-Type-Options", "nosniff") // Disable sniffing
+	if len(Statuscode) == 1 {
+		w.WriteHeader(Statuscode[0])
+	}
+
+	// Header
+	template.Must(template.ParseFiles("templates/header.html")).Execute(w, nil)
+
+	// Now add the passedTemplate
+	fmt.Fprintf(w, "%s", string(passedTemplate.Bytes())) // %s = the uninterpreted bytes of the string or slice
+
+	// And now we execute the footer
+	template.Must(template.ParseFiles("templates/footer.html")).Execute(w, nil)
+}
